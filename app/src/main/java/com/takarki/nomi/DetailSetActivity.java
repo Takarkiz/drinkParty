1610484c1c3e3b7f1ed7854ba5b698bf3fcaf886
Takarkiz <ponpontanuki16@gmail.com>
2017-02-26 10:20:31 +0900

    add Firebase

--- app/src/main/java/com/takarki/nomi/DetailSetActivity.java
+++ app/src/main/java/com/takarki/nomi/DetailSetActivity.java
@@ -3,11 +3,11 @@ package com.takarki.nomi;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class detailSettingActivity extends AppCompatActivity {
+public class DetailSetActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_detail_setting);
+        setContentView(R.layout.activity_detail_set);
     }
 }
