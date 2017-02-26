8805d6d3a0c9ba1416ac09249e9d050d3b9a4a1a
Takarkiz <ponpontanuki16@gmail.com>
2017-02-26 20:56:35 +0900

    add dialog

---
+++ app/src/main/java/com/takarki/nomi/DateDialogFragment.java
@@ -0,0 +1,10 @@
+package com.takarki.nomi;
+
+import android.app.DialogFragment;
+
+/**
+ * Created by Takarki on 2017/02/26.
+ */
+
+public class DateDialogFragment extends DialogFragment {
+}
---
+++ app/src/main/java/com/takarki/nomi/FirebaseSend.java
@@ -0,0 +1,8 @@
+package com.takarki.nomi;
+
+/**
+ * Created by Takarki on 2017/02/26.
+ */
+
+public class FirebaseSend {
+}
